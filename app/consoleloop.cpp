ef647eea4afc8e61f13055052697ff5329f78b76
Richard Edgar <freesurfer.rge@gmail.com>
2018-11-24 16:03:06 -0500

    Working on tokenizer

--- app/consoleloop.cpp
+++ app/consoleloop.cpp
@@ -1,19 +1,52 @@
 #include <iostream>
+#include <sstream>
+#include <locale>
 
 #include "consoleloop.hpp"
 
 namespace Signalbox {
+  void stringtotokens( const std::string s, std::vector<std::string>& tokens ) {
+    tokens.clear();
+
+    std::stringstream nxt;
+    for( auto c=s.begin(); c!=s.end(); ++c ) {
+      if( std::isspace(*c) ) {
+	if( nxt.tellp() != 0 ) {
+	  // Just completed a token
+	  tokens.push_back(nxt.str());
+	  nxt.str(std::string());
+	}
+      } else {
+	nxt << (*c);
+      }
+    }
+    // Need to catch case where token ends line
+    if( nxt.tellp() != 0 ) {
+      // Just completed a token
+      tokens.push_back(nxt.str());
+    }
+  }
+
+  
   void consoleloop( std::map<ItemId,std::unique_ptr<SignalHead>>& sigs ) {
     bool done = false;
 
     while( !done ) {
       std::string inputLine;
 
-      std::cin >> inputLine;
+      std::getline( std::cin, inputLine );
 
       if( inputLine == "q" ) {
 	std::cout << "Received quit" << std::endl;
 	done = true;
+      } else {
+	std::vector<std::string> tokens;
+	stringtotokens(inputLine, tokens);
+
+	for( auto it=tokens.begin(); it!=tokens.end(); ++it ) {
+	  std::cout << (*it) << "==";
+	}
+	std::cout << std::endl;
       }
     }
   }
