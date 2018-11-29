8cb57dcbb326da17446717e377d99a7864175982
Richard Edgar <freesurfer.rge@gmail.com>
2018-11-28 22:18:12 -0500

    Adding explanation of workings

--- app/outputselector.hpp
+++ app/outputselector.hpp
@@ -7,6 +7,23 @@
 #include "outputpin.hpp"
 
 namespace Signalbox {
+  /*
+    Use a trick to enable different output destinations to register
+    themselves at runtime
+
+    The selector for each destination is derived from this abstract
+    class and calls AddSelector in its constructor to register itself
+    into the static map 'selectors' held by this class.
+
+    Each of those selectors also defines a static object of itself in
+    its source file. The runtime will construct each of these before
+    calling main, and the constructors will register them into the
+    'selectors' map
+
+    The main program can then query the static methods of this
+    abstract base class to get information about the output
+    destinations which are available
+   */
   class OutputSelector {
   public:
     static std::string ListOutputSelectors();
