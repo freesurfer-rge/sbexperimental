a3bcbb60ae8895c1b83617bfb2f4c843fa437a84
Richard Edgar <freesurfer.rge@gmail.com>
2018-12-01 20:53:41 -0500

    Another change...

--- tst/mockoutputpintest.cpp
+++ tst/mockoutputpintest.cpp
@@ -10,7 +10,7 @@ BOOST_AUTO_TEST_CASE( Manufacture )
 {
   Signalbox::OutputPin::sample = std::unique_ptr<Signalbox::OutputPin>(new Signalbox::MockOutputPin());
 
-  const char anyPin = 10;
+  const std::string anyPin = "10";
 
   auto res = Signalbox::OutputPin::create(anyPin);
 
@@ -27,7 +27,7 @@ BOOST_AUTO_TEST_CASE( TurnOnOff )
 {
   Signalbox::OutputPin::sample = std::unique_ptr<Signalbox::OutputPin>(new Signalbox::MockOutputPin());
 
-  const char anyPin = 11;
+  const std::string anyPin = "GPIO11";
 
   auto res = Signalbox::OutputPin::create(anyPin);
   
