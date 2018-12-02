0cd4c14ca697efb6e77119e27deddc08ae888217
R Edgar <freesurfer.rge@gmail.com>
2018-12-02 17:20:18 -0500

    Delete copy constructor and operator= for the MockPinManagerFixture This silences a compiler warning due to the pointer member

--- tst/mockpinmanagerfixture.hpp
+++ tst/mockpinmanagerfixture.hpp
@@ -11,4 +11,9 @@ struct MockPinManagerFixture {
     pm() {
     this->pm = &(this->mpm);
   }
+
+  // Delete copy constructor and operator=
+  // This removes a compiler warning due to the pointer member
+  MockPinManagerFixture(MockPinManagerFixture&) = delete;
+  MockPinManagerFixture& operator=(MockPinManagerFixture&) = delete;
 };
