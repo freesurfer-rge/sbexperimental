6eb08d9ea6b0e56e3c890d337e1a98f649e98ab7
Richard Edgar <freesurfer.rge@gmail.com>
2018-12-02 16:23:34 -0500

    Switching SignalHead tests to use PinManager

--- include/mockpinmanager.hpp
+++ include/mockpinmanager.hpp
@@ -16,7 +16,10 @@ namespace Signalbox {
     virtual DigitalOutputPin* CreateDigitalOutputPin(const std::string pinId) override;
 
     MockDigitalOutputPin* FetchMockDigitalOutputPin(const std::string pinId) const;
-    
+
+    size_t DigitalOutputPinCount() const {
+      return this->outputPins.size();
+    }
   private:
     std::map<std::string,std::unique_ptr<MockDigitalOutputPin>> outputPins;
   };
