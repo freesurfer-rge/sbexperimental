2030fadecbd8e942fd8d161a1fadfa1429cd5843
Richard Edgar <freesurfer.rge@gmail.com>
2018-12-22 05:32:33 -0500

    Don't need elaborate checking for the ConsolePinManager

--- app/consolepinmanagerfactory.hpp
+++ app/consolepinmanagerfactory.hpp
@@ -7,25 +7,10 @@
 namespace Signalbox {
   class ConsolePinManagerFactory : public PinManagerFactory {
   public:
-    ConsolePinManagerFactory() : cpm() {}
+    ConsolePinManagerFactory() {}
     
     virtual std::unique_ptr<PinManager> Create() override {
-      if( this->cpm != NULL ) {
-	throw std::runtime_error("Tried to create second ConsolePinManager");
-      }
-      auto res = std::unique_ptr<PinManager>(new ConsolePinManager());
-      this->cpm = dynamic_cast<ConsolePinManager*>(res.get());
-      if( this->cpm == NULL ) {
-	throw std::runtime_error("Failed to cast back to ConsolePinManager");
-      }
-      
-      return res;
+      return std::unique_ptr<PinManager>(new ConsolePinManager());
     }
-
-    ConsolePinManager* cpm;
-
-    // Delete copy constructor and operator= due to pointer member
-    ConsolePinManagerFactory(ConsolePinManagerFactory&) = delete;
-    ConsolePinManagerFactory& operator=(ConsolePinManagerFactory&) = delete;
   };
 }
