95a7a2876e93a25ee3fb82919600ab4d18e7223d
Richard Edgar <freesurfer.rge@gmail.com>
2018-12-28 13:59:24 -0500

    Starting to figure out the required RPC

--- app/cppcmsloop.cpp
+++ app/cppcmsloop.cpp
@@ -7,9 +7,40 @@
 #include <cppcms/applications_pool.h>
 #include <cppcms/rpc_json.h>
 
+#include "signalhead.hpp"
+
 #include "cppcmsloop.hpp"
 
 namespace Signalbox {
+  class SignalHeadService : public cppcms::rpc::json_rpc_server {
+  public:
+    SignalHeadService(cppcms::service& srv)
+      : cppcms::rpc::json_rpc_server(srv) {
+      bind("set", cppcms::rpc::json_method(&SignalHeadService::set, this), method_role);
+    }
+
+    void set( int id, int aspect, int flashing ) {
+      ItemId myId(id);
+
+      auto ci = this->cim->GetById(myId);
+      if( ci == NULL ) {
+	return_error("No such item");
+      }
+      auto sh = dynamic_cast<SignalHead*>(ci);
+      if( sh == NULL ) {
+	return_error("Item is not a SignalHead");
+      }
+
+      auto a = static_cast<SignalAspect>(aspect);
+      auto f = static_cast<SignalFlash>(flashing);
+      
+      sh->SetState(a, f);
+      return_result("State set");
+    }
+    
+    ControlledItemManager* cim;
+  };
+  
   void CppCMSLoop( ControlledItemManager* cim ) {
     std::string cppcmsConfigFile = "config.js";
     cppcms::json::value config;
