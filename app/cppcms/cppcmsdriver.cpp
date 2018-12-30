476fba325b18196184b9a67e1c68675171a23292
Richard Edgar <freesurfer.rge@gmail.com>
2018-12-30 09:05:17 -0500

    Allowing the new HTML page to be served by CPPCMS

--- app/cppcms/cppcmsdriver.cpp
+++ app/cppcms/cppcmsdriver.cpp
@@ -11,7 +11,12 @@ namespace Signalbox {
     config["service"]["ip"] = "0.0.0.0"; // Use 127.0.0.1 to restrict access to localhost
     config["service"]["api"] = "http"; // Be a webserver
     config["service"]["port"] = 8080;
+    config["http"]["script"] = "/rpc";
     config["logging"]["level"] = "debug";
+
+    // Enable serving of plain documents from the 'html' directory
+    config["file_server"]["enable"] = true;
+    config["file_server"]["document_root"] = "html";
 
     booster::shared_ptr<ControlledItemServiceFactory> cisFactory(new ControlledItemServiceFactory(f));
     cppcms::service srv(config);
--- app/cppcms/cppcmsdriver.hpp
+++ app/cppcms/cppcmsdriver.hpp
@@ -6,8 +6,8 @@ namespace Signalbox {
   class CPPCMSDriver : public ControlledItemDriver {
   public:
     // Some sample commands:
-    // curl http://loki:8080 -H "Content-Type: application/json" --data '{ "method":"setsignalhead", "params": [ "00:00:00:02", "Yellow", "Steady" ], "id" : 1}'
-    // curl http://loki:8080 -H "Content-Type: application/json" --data '{ "method":"getsignalhead", "params": [ "00:00:00:02" ], "id" : 1}'
+    // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"setsignalhead", "params": [ "00:00:00:02", "Yellow", "Steady" ], "id" : 1}'
+    // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"getsignalhead", "params": [ "00:00:00:02" ], "id" : 1}'
     virtual void Run(ControlledItemFetcher* f) override;
   };
 }
