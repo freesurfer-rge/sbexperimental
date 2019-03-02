49939cb3c82f7738d4eefab02ea058bc48c10e5a
Richard Edgar <freesurfer.rge@gmail.com>
2019-03-02 16:07:23 -0500

    Need some brief waits in the TrackCircuitMonitor tests to allow the internal thread to update

--- tst/trackcircuitmonitortest.cpp
+++ tst/trackcircuitmonitortest.cpp
@@ -6,6 +6,15 @@
 
 // ============================================
 
+void PauseForTrackCircuitMonitor() {
+  // We don't have visibility into the internal thread
+  // of the TrackCircuitMonitor
+  // In testing, we wait a short time to allow for updates
+  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+}
+
+// ============================================
+
 BOOST_FIXTURE_TEST_SUITE( TrackCircuitMonitor, MockTrackCircuitMonitorFactoryFixture )
 
 BOOST_AUTO_TEST_CASE( LifeCycle )
@@ -33,7 +42,7 @@ BOOST_AUTO_TEST_CASE( LifeCycle )
   BOOST_CHECK_EQUAL( pin->createSettings.at("SomeKey"), "SomeValue" );
 }
 
-BOOST_AUTO_TEST_CASE( SetToOccupied )
+BOOST_AUTO_TEST_CASE( SetToOccupiedAndUnoccupied )
 {
   const std::string pinId = "GPIO25";
   Signalbox::TrackCircuitMonitorData tcmd;
@@ -48,13 +57,55 @@ BOOST_AUTO_TEST_CASE( SetToOccupied )
 
   auto pin = this->mpm.FetchMockDigitalInputPin( pinId );
   BOOST_REQUIRE( pin );
-
   BOOST_CHECK_EQUAL( tcm->Get(), false );
+  
   pin->Set( true );
+  PauseForTrackCircuitMonitor();
   BOOST_CHECK_EQUAL( tcm->Get(), true );
-
   BOOST_CHECK_EQUAL( this->rtc.lastItem, tcmd.id );
   BOOST_CHECK_EQUAL( this->rtc.lastOccupy, true );
+
+  this->rtc.lastItem = 0;
+
+  pin->Set( false );
+  PauseForTrackCircuitMonitor();
+  BOOST_CHECK_EQUAL( tcm->Get(), false );
+  BOOST_CHECK_EQUAL( this->rtc.lastItem, tcmd.id );
+  BOOST_CHECK_EQUAL( this->rtc.lastOccupy, false );
+}
+
+BOOST_AUTO_TEST_CASE( CheckSingleNotification )
+{
+  const std::string pinId = "GPIO01";
+  Signalbox::TrackCircuitMonitorData tcmd;
+  tcmd.id = Signalbox::ItemId::Random();
+  tcmd.inputPin.id = pinId;
+
+  std::unique_ptr<Signalbox::TrackCircuitMonitor> tcm;
+  tcm = this->CreateTrackCircuitMonitor(tcmd);
+  BOOST_REQUIRE( tcm );
+  tcm->Activate();
+  BOOST_CHECK_EQUAL( tcm->Get(), false );
+
+  auto pin = this->mpm.FetchMockDigitalInputPin( pinId );
+  BOOST_REQUIRE( pin );
+
+  pin->Set( true );
+  PauseForTrackCircuitMonitor();
+  BOOST_CHECK_EQUAL( tcm->Get(), true );
+  BOOST_CHECK_EQUAL( this->rtc.lastItem, tcmd.id );
+  BOOST_CHECK_EQUAL( this->rtc.lastOccupy, true );
+
+  // Deliberately change rtc
+  this->rtc.lastItem = 0;
+  this->rtc.lastOccupy = false;
+
+  pin->Set( true );
+  PauseForTrackCircuitMonitor();
+  BOOST_CHECK_EQUAL( tcm->Get(), true );
+  // rtc should be unchanged since this was a repeat
+  BOOST_CHECK_EQUAL( this->rtc.lastItem, 0 );
+  BOOST_CHECK_EQUAL( this->rtc.lastOccupy, false );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
