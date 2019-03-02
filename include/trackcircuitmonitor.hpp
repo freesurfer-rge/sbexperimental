70cc697669abc29350a813b2098dcfc254f58afd
Richard Edgar <freesurfer.rge@gmail.com>
2019-03-01 19:24:18 -0500

    Starting work on the track circuit monitor object itself

---
+++ include/trackcircuitmonitor.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+#include "controlleditem.hpp"
+
+#include "digitalinputpin.hpp"
+#include "railtrafficcontrolclient.hpp"
+
+namespace Signalbox {
+  class TrackCircuitMonitor : public ControlledItem {
+  public:
+    ~TrackCircuitMonitor() {
+      if( this->t.joinable() ) {
+	this->done = true;
+	this->t.join();
+      }
+    }
+
+    virtual void Activate() override;
+    
+    bool Get() const;
+
+    void Run();
+    
+    virtual std::string getTypeString() const override {
+      return std::string("trackcircuitmonitor");
+    }
+  private:
+    friend class TrackCircuitMonitorFactory;
+
+    TrackCircuitMonitor(const ItemId id) :
+      ControlledItem(id),
+      done(false),
+      pin(),
+      rtc(),
+      pollInterval(std::chrono::seconds(2)) {}
+    
+    std::atomic<bool> done;
+    DigitalInputPin* pin;
+    RailTrafficControlClient* rtc;
+    std::chrono::duration<int,std::milli> pollInterval;
+    
+    std::thread t;
+  };
+}
