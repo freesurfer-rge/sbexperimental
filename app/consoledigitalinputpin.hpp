951c4e84521faf36584de0d1bd1448bbaf0bc207
Richard Edgar <freesurfer.rge@gmail.com>
2019-03-03 13:54:39 -0500

    Working on the app. Needed a way to see that things were triggering when the input pin changed state

--- app/consoledigitalinputpin.hpp
+++ app/consoledigitalinputpin.hpp
@@ -1,6 +1,6 @@
 #pragma once
-
 #include <string>
+#include <thread>
 
 #include "digitalinputpin.hpp"
 
@@ -12,15 +12,31 @@ namespace Signalbox {
     ConsoleDigitalInputPin() :
       DigitalInputPin(),
       state(false),
-      id() {}
+      id(),
+      done(false),
+      t() {
+      this->t = std::thread(&ConsoleDigitalInputPin::Run, this);
+    }
+
+    ~ConsoleDigitalInputPin() {
+      if( this->t.joinable() ) {
+	this->done = true;
+	this->t.join();
+      }
+    }
     
     virtual bool Get() const override;
 
     void Set(const bool level);
+
+    void Run();
   private:
     friend class ConsolePinManager;
 
     bool state;
     std::string id;
+
+    std::atomic<bool> done;
+    std::thread t;
   };
 }
