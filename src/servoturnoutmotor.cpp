2c8f73c8ff800c5f37052fd13c7ac553b7e32f43
Richard Edgar <freesurfer.rge@gmail.com>
2019-06-09 08:33:02 -0400

    Add tests of the ServoTurnoutMotor itself This involved implementing the require functionality

--- src/servoturnoutmotor.cpp
+++ src/servoturnoutmotor.cpp
@@ -1,7 +1,20 @@
+#include <sstream>
+#include <stdexcept>
+
 #include "servoturnoutmotor.hpp"
 
 namespace Signalbox {
   void ServoTurnoutMotor::SetState(const TurnoutState desired) {
-    throw std::runtime_error("Not implemented");
+    if( desired == TurnoutState::Straight ) {
+      this->servo->Set(pwmStraight);
+      this->state = TurnoutState::Straight;
+    } else if( desired == TurnoutState::Curved ) {
+      this->servo->Set(pwmCurved);
+      this->state = TurnoutState::Curved;
+    } else {
+      std::stringstream msg;
+      msg << "Unsupported state: " << desired;
+      throw std::runtime_error(msg.str());
+    }
   }
 }
