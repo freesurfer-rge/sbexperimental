568c6f238eb2e22db6d558c299c20b069621206f
Richard Edgar <freesurfer.rge@gmail.com>
2019-06-09 15:39:36 -0400

    Fill out some more sample commands

--- app/cppcms/cppcmsdriver.hpp
+++ app/cppcms/cppcmsdriver.hpp
@@ -6,10 +6,21 @@ namespace Signalbox {
   class CPPCMSDriver : public ControlledItemDriver {
   public:
     // Some sample commands:
+
+    // List everything
     // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"listitems", "params":[], "id":1 }'
+
+    // SignalHeads
     // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"setsignalhead", "params": [ "00:00:00:02", "Yellow", "Steady" ], "id" : 1}'
     // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"getsignalhead", "params": [ "00:00:00:02" ], "id" : 1}'
+
+    // TrackCircuitMonitors
     // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"gettrackcircuitmonitor", "params" : [ "00:00:00:aa" ], "id" : 1}'
+
+    // TurnoutMotors
+    // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"getturnoutmotor", "params" : [ "00:fe:1a:af" ], "id" : 1}'
+    // curl http://loki:8080/rpc -H "Content-Type: application/json" --data '{ "method":"setturnoutmotor", "params" : [ "00:fe:1a:af", "Curved" ], "id" : 1}'
+
     virtual void Run(ControlledItemFetcher* f) override;
   };
 }
