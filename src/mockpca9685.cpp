abc23c318a0ead147e264b1b2b6b6b32f2c60eff
Richard Edgar <freesurfer.rge@gmail.com>
2019-06-11 15:47:08 -0400

    Fill out functionality to get test to work

--- src/mockpca9685.cpp
+++ src/mockpca9685.cpp
@@ -5,10 +5,29 @@
 
 namespace Signalbox {
   void MockPCA9685::Initialise( const std::map<std::string,std::string>& settings ) {
-    std::stringstream msg;
-    msg << __PRETTY_FUNCTION__
-	<< " not implemented";
-    throw std::runtime_error(msg.str());
+    const std::string refClockSetting = "referenceClock";
+    const std::string pwmFreqSetting = "pwmFrequency";
+
+    if( settings.count(refClockSetting) != 1 ) {
+      std::stringstream msg;
+      msg << "Settings for MockPCA9685 did not contain " << refClockSetting;
+      throw std::out_of_range(msg.str());
+    }
+    if( settings.count(pwmFreqSetting) != 1 ) {
+      std::stringstream msg;
+      msg << "Settings for MockPCA9685 did not contain " << pwmFreqSetting;
+      throw std::out_of_range(msg.str());
+    }
+
+    this->referenceClock = std::stod(settings.at(refClockSetting));
+    this->pwmFrequency = std::stod(settings.at(pwmFreqSetting));
+
+    if( this->referenceClock <= 0 ) {
+      throw std::out_of_range("referenceClock must be positive"); 
+    }
+    if( this->pwmFrequency <= 0 ) {
+      throw std::out_of_range("pwmFrequency must be positive");
+    }
   }
 
   PWMChannel* MockPCA9685::GetPWMChannel( const std::string channelId,
